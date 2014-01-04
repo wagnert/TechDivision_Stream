057b6909190535a35def2d2b8a7d957c92cfd3b3
Philipp Dittert <p.dittert@techdivision.com>
2014-01-04 09:40:49 +0100

    format file and class documentation to psr-2 standard

--- src/TechDivision/Stream/Client.php
+++ src/TechDivision/Stream/Client.php
@@ -1,13 +1,15 @@
 <?php
-
 /**
  * TechDivision\Stream\Client
  *
- * NOTICE OF LICENSE
+ * PHP version 5
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * @category  Appserver.io
+ * @package   TechDivision_Socket
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2013 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io
  */
 
 namespace TechDivision\Stream;
@@ -18,13 +20,16 @@ use TechDivision\Stream;
  * The client socket implementation. This implementation can be used for creating a client socket implementation
  * only. To create a socket server that listens to a address/port use the {@link \TechDivision\Socket\Server Server} class.
  *
- * @package     TechDivision\Stream
- * @copyright  	Copyright (c) 2010 <info@techdivision.com> - TechDivision GmbH
- * @license    	http://opensource.org/licenses/osl-3.0.php
- *              Open Software License (OSL 3.0)
- * @author      Tim Wagner <tw@techdivision.com>
+ * @category  Appserver.io
+ * @package   TechDivision_Stream
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2013 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io
  */
-class Client extends Stream {
+
+class Client extends Stream
+{
 
     /**
      *  The number of bytes to send/receive.
@@ -41,11 +46,11 @@ class Client extends Stream {
     /**
      * Initialize the client socket with the IP address and port.
      *
-     * @param string $address The IP address to initialize the socket with
-     * @param integer $port The port to initialize the socket with
-     * @return void
+     * @param string  $address The IP address to initialize the socket with
+     * @param integer $port    The port to initialize the socket with
      */
-    public function __construct($address = '0.0.0.0', $port = 0) {
+    public function __construct($address = '0.0.0.0', $port = 0)
+    {
         $this->setAddress($address);
         $this->setPort($port);
     }
@@ -55,7 +60,8 @@ class Client extends Stream {
      *
      * @return integer The number of bytes to send/receive
      */
-    public function getLineLength() {
+    public function getLineLength()
+    {
         return $this->lineLength;
     }
 
@@ -64,7 +70,8 @@ class Client extends Stream {
      *
      * @return string The new line character
      */
-    public function getNewLine() {
+    public function getNewLine()
+    {
         return $this->newLine;
     }
 
@@ -73,7 +80,8 @@ class Client extends Stream {
      *
      * @return \TechDivision\Socket The socket instance itself
      */
-    public function start() {
+    public function start()
+    {
         return $this->create()->connect();
     }
 
@@ -81,9 +89,11 @@ class Client extends Stream {
      * Sends a line (ends with the new line character) over the socket.
      *
      * @param string $data The data to send
+     *
      * @return integer The number of bytes sent
      */
-    public function sendLine($data) {
+    public function sendLine($data)
+    {
         return $this->send($data . $this->getNewLine());
     }
 
@@ -92,7 +102,8 @@ class Client extends Stream {
      *
      * @return string The data read from the socket
      */
-    public function readLine() {
+    public function readLine()
+    {
             
         // initialize the buffer
         $buffer = '';
@@ -109,7 +120,4 @@ class Client extends Stream {
             }
         }
     }
-
-
-
 }
--- src/TechDivision/Stream/SecureServer.php
+++ src/TechDivision/Stream/SecureServer.php
@@ -1,27 +1,32 @@
 <?php
-
 /**
- * TechDivision\Stream\Server
+ * TechDivision\Stream\SecureServer
  *
- * NOTICE OF LICENSE
+ * PHP version 5
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * @category  Appserver.io
+ * @package   TechDivision_Socket
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2013 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io
  */
+
 namespace TechDivision\Stream;
 
-use TechDivision\Stream\Client;
+use TechDivision\Stream;
 
 /**
  * A secure streaming socket implementation.
  *
- * @package TechDivision\Stream
- * @copyright Copyright (c) 2010 <info@techdivision.com> - TechDivision GmbH
- * @license http://opensource.org/licenses/osl-3.0.php
- *          Open Software License (OSL 3.0)
- * @author Tim Wagner <tw@techdivision.com>
+ * @category  Appserver.io
+ * @package   TechDivision_Stream
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2013 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io
  */
+
 class SecureServer extends Server
 {
     
